3fc5102eb54d0e859d1bac16a91b80a30135de9f
Malte Gerth <malte.gerth@proclima.de>
2015-10-17 16:58:15 +0200

    added first draft of templates

---
+++ doslos_server/doslos/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, url
+from doslos.views import select_level, questionnaire, post_questionnaire
+
+urlpatterns = patterns(
+    '',
+    url(r'', select_level, name='select_level'),
+    url(r'^questionnaire/(?P<level_id>\d+)/$', questionnaire, name='questionnaire'),
+    url(r'^questionnaire/(?P<level_id>\d+)/post/$', post_questionnaire, name='post_questionnaire'),
+)
--- doslos_server/doslos/views.py
+++ doslos_server/doslos/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from doslos.models import Word
+
+
+def select_level(request):
+    return render(request, 'doslos/select_level.html',
+                  context={'available_levels': request.user.get_available_levels()})
+
+
+def questionnaire(request, level_id):
+    word = ''  # FIXME implement
+    answers = []  # FIXME implement
+    context = {
+        'level_id': level_id,
+        'word': word,
+        'answers': answers,
+        'level_right_answer_counter': request.session.get('level_right_answer_counter', 0)
+    }
+    return render(request, 'doslos/questionnaire.html', context=context)
+
+
+def post_questionnaire(request, level_id):
+    level_right_answer_counter = request.session.get('level_right_answer_counter', 0)
+    word = Word.objects.get(pk=request.POST['word_id'])
+    if word.value_de == request.POST['answer']:
+        level_right_answer_counter += 1
+        word.increase_right_answer_counter(request.user)
+    else:
+        level_right_answer_counter = 0
+        word.reset_right_answer_counter(request.user)
+    if level_right_answer_counter >= 10:
+        request.user.complete_level(level_id)
+        level_right_answer_counter = 0
+        request.session['level_right_answer_counter'] = level_right_answer_counter
+        return redirect('select_level')
+    request.session['level_right_answer_counter'] = level_right_answer_counter
+    return redirect('questionaire')
