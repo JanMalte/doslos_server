9f7b7a5edbf02bbd21b5fc0ceb90767e8f397988
Malte Gerth <malte.gerth@proclima.de>
2015-10-17 20:58:45 +0200

    added basic frontend styling and messages

--- doslos_server/doslos/views.py
+++ doslos_server/doslos/views.py
@@ -1,5 +1,7 @@
+from django.contrib import messages
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect
+from django.utils.translation import ugettext as _
 
 from doslos.models import Word, Level
 
@@ -15,9 +17,11 @@ def questionnaire(request, level_id):
     answers = level.get_possible_answers(word, request.user)
     context = {
         'level_id': level_id,
+        'level': level,
         'word': word,
         'answers': answers,
-        'level_right_answer_counter': request.session.get('level_right_answer_counter', 0)
+        'level_right_answer_counter': request.session.get('level_right_answer_counter', 0),
+        'completed_percentage': request.session.get('level_right_answer_counter', 0) / 10 * 100
     }
     return render(request, 'doslos/questionnaire.html', context=context)
 
@@ -29,13 +33,16 @@ def post_questionnaire(request, level_id):
     if word.value_de == request.POST['answer']:
         level_right_answer_counter += 1
         word.increase_right_answer_counter(request.user)
+        messages.success(request, _('The answer was correct'))
     else:
         level_right_answer_counter = 0
         word.reset_right_answer_counter(request.user)
+        messages.warning(request, _('The answer was not correct. The correct answer is "%s"') % word.value_de)
     if level_right_answer_counter >= 10:
         request.user.complete_level(level)
         level_right_answer_counter = 0
         request.session['level_right_answer_counter'] = level_right_answer_counter
+        messages.success(request, _('You completed the level "%s"') % level.name)
         return redirect(reverse('select_level'))
     request.session['level_right_answer_counter'] = level_right_answer_counter
     return redirect(reverse('questionnaire', kwargs={'level_id': int(level_id)}))
