56e7b61c74d8b211d55da2ec15edf7495453cb19
Malte Gerth <malte.gerth@proclima.de>
2015-10-17 21:06:39 +0200

    added simple error handling if no answer is given or word does not exist

--- doslos_server/doslos/views.py
+++ doslos_server/doslos/views.py
@@ -29,20 +29,23 @@ def questionnaire(request, level_id):
 def post_questionnaire(request, level_id):
     level = Level.objects.get(pk=level_id)
     level_right_answer_counter = request.session.get('level_right_answer_counter', 0)
-    word = Word.objects.get(pk=request.POST['word_id'])
-    if word.value_de == request.POST['answer']:
-        level_right_answer_counter += 1
-        word.increase_right_answer_counter(request.user)
-        messages.success(request, _('The answer was correct'))
-    else:
-        level_right_answer_counter = 0
-        word.reset_right_answer_counter(request.user)
-        messages.warning(request, _('The answer was not correct. The correct answer is "%s"') % word.value_de)
-    if level_right_answer_counter >= 10:
-        request.user.complete_level(level)
-        level_right_answer_counter = 0
+    try:
+        word = Word.objects.get(pk=request.POST.get('word_id', 0))
+        if word.value_de == request.POST.get('answer', ''):
+            level_right_answer_counter += 1
+            word.increase_right_answer_counter(request.user)
+            messages.success(request, _('The answer was correct'))
+        else:
+            level_right_answer_counter = 0
+            word.reset_right_answer_counter(request.user)
+            messages.warning(request, _('The answer was not correct. The correct answer is "%s"') % word.value_de)
+        if level_right_answer_counter >= 10:
+            request.user.complete_level(level)
+            level_right_answer_counter = 0
+            request.session['level_right_answer_counter'] = level_right_answer_counter
+            messages.success(request, _('You completed the level "%s"') % level.name)
+            return redirect(reverse('select_level'))
         request.session['level_right_answer_counter'] = level_right_answer_counter
-        messages.success(request, _('You completed the level "%s"') % level.name)
-        return redirect(reverse('select_level'))
-    request.session['level_right_answer_counter'] = level_right_answer_counter
+    except Word.DoesNotExist:
+        pass
     return redirect(reverse('questionnaire', kwargs={'level_id': int(level_id)}))
